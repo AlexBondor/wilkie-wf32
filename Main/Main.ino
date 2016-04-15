c3782c5fad00ca8b15a6aa0b503408ad8abc78fa
alexbondor <alexandru.viorel.bondor@gmail.com>
2016-04-15 20:48:22 +0300

    Bluetooth remote control working

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,17 +1,22 @@
 #include "Robot.h"
 
 Robot robot;
-
+  
 void setup()
 {  
-  robot.moveForward(20);
-  robot.turnRight(90);
-  robot.moveBackward(10);
-  robot.turnLeft(90);
+  //robot.moveForward(200);
+  //robot.startVacuum();
+
+  // Serial1 is used by Bluetooth module to receive data
+  Serial1.begin(9600);
 }
 
 void loop()
 {
   robot.process();
+
+  // Time to go now..
+  // There is so much code I need to write.
+  // Thank you for checking out the video!
 }
 

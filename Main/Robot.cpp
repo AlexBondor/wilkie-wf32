538b7675e3407919be3ad43053a3465a2c6d084f
alexbondor <alexandru.viorel.bondor@gmail.com>
2016-04-15 20:43:44 +0300

    Added remote control support

--- Main/Robot.cpp
+++ Main/Robot.cpp
@@ -1,13 +1,86 @@
 // Include this library's description file
 #include "Robot.h"
 
+static Robot* instance;
+static bool _enableRemoteControlFlag;
+static bool _enablePumpFlag;
+static bool _moveForwardFlag;
+static bool _moveBackwardFlag;
+static bool _turnLeftFlag;
+static bool _turnRightFlag;
+
+// Callback methods called by bluetooth module
+void Robot::enableRemoteControlCallback()
+{
+  instance->resetFlags();
+  _enableRemoteControlFlag = !_enableRemoteControlFlag;
+  instance->updateMovement();
+}
+
+void Robot::enablePumpCallback()
+{
+  _enablePumpFlag = !_enablePumpFlag;
+  if (_enablePumpFlag)
+  {
+    instance->startVacuum();
+  }
+  else
+  {
+    instance->stopVacuum();
+  }
+}
+
+void Robot::moveForwardCallback()
+{
+  instance->resetFlags();
+  _moveForwardFlag = !_moveForwardFlag;
+  instance->updateMovement();
+}
+
+void Robot::moveBackwardCallback()
+{
+  instance->resetFlags();
+  _moveBackwardFlag = !_moveBackwardFlag;
+  instance->updateMovement();
+}
+
+void Robot::turnLeftCallback()
+{
+  instance->resetFlags();
+  _turnLeftFlag = !_turnLeftFlag;
+  instance->updateMovement();
+}
+
+void Robot::turnRightCallback()
+{
+  instance->resetFlags();
+  _turnRightFlag = !_turnRightFlag;
+  instance->updateMovement();
+}
+
 /*
  * Robot constructor
  */
 Robot::Robot()
 {
+  instance = this;
+  _enableRemoteControlFlag = false;
+  _enablePumpFlag = false;
+  _moveForwardFlag = false;
+  _moveBackwardFlag = false;
+  _turnLeftFlag = false;
+  _turnRightFlag = false;
+  
   // Initialize motor controller
   _motorController.init();
+
+  // Set the callback methods for when the bluetooth module receives data
+  _bluetoothModule.setEnableRemoteControlCallbackMethod(enableRemoteControlCallback);
+  _bluetoothModule.setEnablePumpCallbackMethod(enablePumpCallback);
+  _bluetoothModule.setMoveForwardCallbackMethod(moveForwardCallback);
+  _bluetoothModule.setMoveBackwardCallbackMethod(moveBackwardCallback);
+  _bluetoothModule.setTurnLeftCallbackMethod(turnLeftCallback);
+  _bluetoothModule.setTurnRightCallbackMethod(turnRightCallback);
 }
 
 Point Robot::getPosition()
@@ -18,6 +91,7 @@ Point Robot::getPosition()
 void Robot::process()
 {
   _motorController.process();
+  _bluetoothModule.listen();
 }
 
 void Robot::moveForward(double distance)
@@ -38,5 +112,55 @@ void Robot::turnLeft(int degrees)
 void Robot::turnRight(int degrees)
 {
   _motorController.turnRight(degrees);
+}
+
+void Robot::brake()
+{
+  _motorController.brake();
+}
+
+void Robot::startVacuum()
+{
+  _motorController.startVacuum();
+}
+
+void Robot::stopVacuum()
+{
+  _motorController.stopVacuum();
+}
+
+void Robot::resetFlags()
+{
+  _moveForwardFlag = false;
+  _moveBackwardFlag = false;
+  _turnLeftFlag = false;
+  _turnRightFlag = false;  
+}
+
+/*
+ * Updates the motion of the robot taking into account
+ * the flags changed by the bluetooth module upron data 
+ * receival
+ */
+void Robot::updateMovement()
+{
+  // Only one command executed at once
+  _motorController.brake();
+  if (_moveForwardFlag)
+  {
+    moveForward(100);
+  }
+  if (_moveBackwardFlag)
+  {
+    moveBackward(100);    
+  }
+  if (_turnLeftFlag)
+  {
+    turnLeft(360);
+  }
+  if (_turnRightFlag)
+  {
+    turnRight(360);
+  }
 }
 
--- Main/Robot.h
+++ Main/Robot.h
@@ -4,6 +4,7 @@
 
 #include "Arduino.h"
 #include "MotorController.h"
+#include "BluetoothModule.h"
 
 class Robot
 {
@@ -15,8 +16,23 @@ class Robot
     void moveBackward(double distance);
     void turnLeft(int degrees);
     void turnRight(int degrees);
+    void startVacuum();
+    void stopVacuum();
+    void brake();
 
   private:
+    void resetFlags();
     MotorController _motorController;
+    BluetoothModule _bluetoothModule;
+
+    // Callbacks called by bluetooth module
+    static void enableRemoteControlCallback();
+    static void enablePumpCallback();
+    static void moveForwardCallback();
+    static void moveBackwardCallback();
+    static void turnLeftCallback();
+    static void turnRightCallback();
+
+    void updateMovement();
 };
 #endif

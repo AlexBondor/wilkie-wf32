3629f80883b0cdd23aebd865d87dd402f14b5eac
alexbondor <alexandru.viorel.bondor@gmail.com>
2016-04-19 00:51:02 +0300

    Scanning for wifi networks working

--- Main/Controller/WifiController.cpp
+++ Main/Controller/WifiController.cpp
@@ -7,6 +7,7 @@
 
 TCPSocket _tcpSocket;
 STATE state = NONE;
+IPSTATUS status = ipsSuccess;
 
 unsigned _tStart = 0;
 unsigned _tWait = 5000;
@@ -31,6 +32,61 @@ int _messageToWriteSize = 0;
 byte _messageToRead[1024];
 int _messageToReadSize = 0;
 
+/*
+ *  Makes a scan of the available WiFi networks
+ *  Returns a list of the names of the networks and binds
+ *  the function argument to the number of available networks
+ */
+char** WifiController::scan(int &size)
+{
+  char** result;
+  while(!deIPcK.wfScan(&size, &status))
+  {
+    DEIPcK::periodicTasks();
+  }
+
+  #ifdef WIFI_DEBUG
+  Serial.println(size);
+  #endif
+
+  result = new char*[size];
+  if(IsIPStatusAnError(status))
+  {
+    #ifdef WIFI_DEBUG
+    Serial.println("Scan Failed");
+    #endif
+  }
+  SCANINFO scanInfo;
+  for (int i = 0; i < size; i++)
+  {
+    if(deIPcK.getScanInfo(i, &scanInfo))
+    {
+      #ifdef WIFI_DEBUG
+      Serial.print("Scan info for index: ");
+      Serial.println(i, DEC);
+      Serial.print("SSID: ");
+      #endif
+
+      result[i] = new char[scanInfo.ssidLen];
+      for (int j = 0; j < scanInfo.ssidLen; j++)
+      {
+        result[i][j] = (char) scanInfo.ssid[j];
+      }
+    }
+    else
+    {
+      result[i] = "Unavailable";
+    }
+  }
+  return result;
+}
+
+/*
+ *  This method must be called each loop cycle in order
+ *  for the WiFi module to be responsive.
+ *  Switch case taken from the TCPEchoClient example application
+ *  provided by Digilent
+ */
 void WifiController::process()
 {
 	IPSTATUS status;
@@ -175,6 +231,10 @@ void WifiController::process()
   DEIPcK::periodicTasks();
 }
 
+/*
+ *  Tries to establish a connection to the wifi whose details
+ *  are provided as parameters
+ */
 bool WifiController::connectToWifi(char* wifiName, char* password)
 {
 	state = CONNECT;
@@ -187,6 +247,12 @@ bool WifiController::connectToWifi(char* wifiName, char* password)
 	return state == CONNECTED;
 }
 
+/*
+ *  Tries to establish a connection to a server whose details are
+ *  provided as parameters.
+ *  Note that a successful WiFi connection must be established in the
+ *  first place.
+ */
 bool WifiController::connectToServer(char* serverIp, int serverPort)
 {
 	state = TCPCONNECT;
@@ -200,6 +266,12 @@ bool WifiController::connectToServer(char* serverIp, int serverPort)
 	return state == TCPCONNECTED;
 }
 
+/*
+ *  Adds a message to be sent to the server to the messages queue.
+ *  The messages are not sent directly because we don't want to make
+ *  the robot unresponsive. We buffer the messages and once we find
+ *  a small window we send all the buffered messages to the server.
+ */
 bool WifiController::writeToServer(char* messageToWrite, int messageToWriteSize)
 {
 	_messageToWriteSize = messageToWriteSize;
--- Main/Main.ino
+++ Main/Main.ino
@@ -1,41 +1,27 @@
 #include "Robot.h"
 
 Robot robot;
-int cou = 10;
-int s = 2;
-char buff[5];
 
 void setup()
 {  
-  //robot.moveForward(200);
-  //robot.startVacuum();
-
   // Serial1 is used by Bluetooth module to receive data
-  // Serial1.begin(9600);
+  Serial1.begin(9600);
   Serial.begin(9600);
   Serial.println("hello");
 
-  robot.connectToWifi("dd-wrt", "qaz123wsx");
-  robot.connectToServer("192.168.1.128", 44300);
+  // robot.connectToWifi("dd-wrt", "qaz123wsx");
+  // robot.connectToServer("192.168.1.128", 44300);
+  int size;
+  char** networks = robot.wifiScan(size);
+  for (int i = 0; i < size; i++)
+  {
+    Serial.println(networks[i]);
+  }
 }
 
 void loop()
 {
-  if (cou > 999)
-  {
-    while(1)
-    {
-      robot.wifiProcess();
-    }
-  }
-  if (cou > 99)
-  {
-    s = 3;
-  }
-  itoa(cou++, buff, 10);
-  robot.wifiProcess();
-  robot.writeToServer(buff, s);
-  delay(10);
+  // robot.writeToServer(buff, s);
   // robot.process();
 
   // Time to go now..

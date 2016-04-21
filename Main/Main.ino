79508a9efecb4590950101c7d55811f471d778ca
alexbondor <alexandru.viorel.bondor@gmail.com>
2016-04-21 19:38:37 +0300

    Robot connects to WiFi, attempts to connect to Processing server and sends sensors data to be displayed

--- Main/Main.ino
+++ Main/Main.ino
@@ -2,38 +2,62 @@
 
 Robot robot;
 
+char xs1[10], ys1[10];
+char xs2[10], ys2[10];
+char xs3[10], ys3[10];
+
+int start;
+
 void setup()
 {  
   // Serial1 is used by Bluetooth module to receive data
   Serial1.begin(9600);
   Serial.begin(19200);
   Serial.println("hello");
-
- // robot.moveForward(50);
-
- // robot.connectToWifi("dd-wrt", "qaz123wsx");
- // robot.connectToServer("192.168.1.128", 44300);
+  robot.connectToWifi("dd-wrt", "qaz123wsx");
+  robot.connectToServer("192.168.1.128", 44300);
 //  int size;
 //  char** networks = robot.wifiScan(size);
 //  for (int i = 0; i < size; i++)
 //  {
 //    Serial.println(networks[i]);
 //  }
+  start = millis();
 }
 
 void loop()
 {
   //robot.writeToServer("10", 2);
   robot.process();
+  sprintf(xs1, "%d", (int)robot.getLeftEyeData().getX());
+  sprintf(ys1, "%d", (int)robot.getLeftEyeData().getY());
+  sprintf(xs2, "%d", (int)robot.getFrontEyeData().getX());
+  sprintf(ys2, "%d", (int)robot.getFrontEyeData().getY());
+  sprintf(xs3, "%d", (int)robot.getRightEyeData().getX());
+  sprintf(ys3, "%d", (int)robot.getRightEyeData().getY());
+  if (millis() - start > 100)
+  {
+    start = millis();
+    robot.writeToServer(xs1, strlen(xs1));
+    robot.writeToServer(ys1, strlen(ys1));
+    robot.writeToServer(xs2, strlen(xs2));
+    robot.writeToServer(ys2, strlen(ys2));
+    robot.writeToServer(xs3, strlen(xs3));
+    robot.writeToServer(ys3, strlen(ys3));
+  }
+  // delay(20);
+  // delay(100);
   // Serial.print(robot.getLeftEyeData().getX());
   // Serial.print(" ");
   // Serial.println(robot.getLeftEyeData().getY());
-  // Serial.println(robot.getLeftEyeRawData());
   // Serial.println("-----------------------");
-  // delay(35);
-  //delay(100);
-  // Time to go now..
-  // There is so much code I need to write.
-  // Thank you for checking out the video!
+  // Serial.print(robot.getFrontEyeData().getX());
+  // Serial.print(" ");
+  // Serial.println(robot.getFrontEyeData().getY());
+  // Serial.println("-----------------------");
+  // Serial.print(robot.getRightEyeData().getX());
+  // Serial.print(" ");
+  // Serial.println(robot.getRightEyeData().getY());
+  // Serial.println("+++++++++++++++++++++++");
 }
 
